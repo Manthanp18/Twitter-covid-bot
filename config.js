7ffceef5fc48a0298b7cb3b4869d21b9e6bf6d3d
Manthan Patel <patelmanthan1806@gmail.com>
2021-04-24 22:37:00 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'YcFsn6jBS2fjSqrdsdTn1O8ym',
+  consumer_secret: 'aXsztQVDosj6mTW8TwqpQjP9Q8bVLmAp7NMTGtloXJL2mAJZfY',
+  access_token: '1384738525124251649-tSAwUE8ERjWRvpINyWfEp6MhLUvjEh',
+  access_token_secret: 'huwQJ2S7jGEnqTpOUnMMc0sw95iIueicJS4ay2oMruz7D'
+}
